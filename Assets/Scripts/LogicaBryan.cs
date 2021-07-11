8617a58ed1d7d93bdb2995bddbca22883b5761e5
E1R0N <82121943+E1R0N@users.noreply.github.com>
2021-07-11 18:43:14 -0500

    New Characters

---
+++ Assets/Scripts/LogicaBryan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPersonaje2 : MonoBehaviour
+{
+    private Animator anim;
+    public float x, y;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+    }
+}
